f9ec0521493d5167156004fe107637dd1badf4a1
Gilles Henrard <gilleshenrard@users.noreply.github.com>
2014-11-14 13:52:58 +0100

    Create command.py
    Created the command class with :
    - a few basic methods
    - constructors
    - a property for _finalCommand
    - a test procedure

---
+++ command.py
@@ -0,0 +1,30 @@
+class command(scriptable):
+	"""Defines specific commands (without formatting)
+	Defines the final command for the tree"""
+	
+	def __init__(self):
+		"""Creates an empty specific command"""
+		
+		scriptable.__init__(self)
+		self.finalCommand = ""
+	
+	def __init__(self, name):
+		"""Creates a specific command"""
+
+		scriptable.__init__(self, name)
+		self._finalCommand = ""
+
+	def _set_finalCommand(self, final):
+		self.finalCommand = final
+
+	def _get_finalCommand(self):
+		return self._finalCommand
+
+	Final = property(_set_finalCommand, _get_finalCommand)
+
+#Test the constructors
+if __name__ == "__main__":
+	test = command("Some command", "Just a command")
+	print(test.Name)
+	print(test.Final)
+	print(test)
