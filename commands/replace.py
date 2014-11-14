64ebc49e38e9dc1ca6feaed976ca8b7a133343d2
Gilles Henrard <gilleshenrard@users.noreply.github.com>
2014-11-14 15:34:07 +0100

    Implementing methods in Replace
    Implemented :
    - constructors
    - getters/setters
    - __str__
    - prepareCommand
    - added property

--- commands/replace.py
+++ commands/replace.py
@@ -1,2 +1,32 @@
 class Replace(File):
 	"""Replaces a line by another in a file"""
+
+	def __init__(self):
+		File.__init__(self)
+		self._oldLine = ""
+
+	def __init__(self, name):
+		File.__init__(self, name)
+		self._oldLine = ""
+
+	def __init__(self, name, newline):
+		File.__init__(self, name, newline)
+		self._oldLine = ""
+
+	def __init(self, name, oldline, newline):
+		File.__init(self, name, newline)
+		self._oldLine = oldline
+
+	def _set_oldLine(self, oldline):
+		self._oldLine = oldline
+
+	def _get_oldLine(self):
+		return self._oldLine
+
+	def __str__(self):
+		return "replacing {} by {} in {}".format(self.oldLine, self.newLine, self.Name)
+
+	def prepareCommand(self):
+		self.Final = "sed -i 's/{}/{}/g' {}".format(self.oldLine, self.newLine, self.Name)
+
+	oldLine = property(_set_oldLine, _get_oldLine)
