4d18e2d296e3b18f928d95ad7203a16da173975c
Gilles Henrard <gilleshenrard@users.noreply.github.com>
2014-11-14 14:32:12 +0100

    Added constructors, methods, made abstract
    Added :
    - 3 constructors
    - getters/setters for _newLine
    - property for _newLine
    - tried to made File abstract by reassigning prepareCommand as abstract

--- file.py
+++ file.py
@@ -1,2 +1,31 @@
+import abc
+
 class File(Command):
 	"""Base class to handle files"""
+	
+	__metaclass__  = abc.ABCMeta
+
+	def __init__(self):
+		Command.__init__(self)
+		self._newLine = ""
+
+	def __init__(self, name):
+		Command.__init__(self, name)
+		self._newLine = ""
+
+	def __init__(self, name, newline)
+		Command.__init__(self, name, comment)
+		self._newLine = newline
+
+	def _get_newLine(self):
+		return self.newLine
+
+	def _set_newLine(self, newline):
+		self._newLine = newline
+		
+	@abc.abstractmethod
+	def prepareCommand(self):
+		"""trying to make File abstract by making prepareCommand abstract"""
+
+	newLine = property(_get_newLine, set_newLine)
+	
