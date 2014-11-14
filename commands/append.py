f644153e9a17ddf3d3dd049faf026bec73e4a998
Gilles Henrard <gilleshenrard@users.noreply.github.com>
2014-11-14 15:20:08 +0100

    Implemented special methods
    Implemented __str__
    Implemented specialised prepareCommand()

--- commands/append.py
+++ commands/append.py
@@ -1,2 +1,18 @@
 class Append(File):
 	"""Appends a line in a file"""
+	
+	def __init__(self):
+		File.__init__(self)
+
+	def __init__(self, name)
+		File.__init__(self, name)
+
+	def __init__(self, name, newline)
+		File.__init__(self, name, newline)
+
+	def __str__(self):
+		return "Appending line {} into file {}".format(self.newLine, self.Name)
+
+	def prepareCommand(self):
+		self.Final = "echo {} >> {}".format(self.newLine, self.Name)
+		return self.Final
