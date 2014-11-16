1a3af726dc5f3827ef5a0ec0c68ac5a1b8e87def
Gilles Henrard <gilleshenrard@users.noreply.github.com>
2014-11-16 05:06:57 +0100

    replaced whole file by something functional

--- commands/fileclass.py
+++ commands/fileclass.py
@@ -2,31 +2,24 @@ from command import *
 import abc
 
 class File(Command):
-	"""Base class to handle files"""
-	
+	"""Abstract base class to handle files
+	Defines :
+	NewLine : New line to output to a file"""
+
 	__metaclass__  = abc.ABCMeta
 
-	def __init__(self):
-		Command.__init__(self)
-		self._newLine = ""
-
-	def __init__(self, name):
-		Command.__init__(self, name)
-		self._newLine = ""
-
-	def __init__(self, name, newline):
+	def __init__(self, name="", newline=""):
 		Command.__init__(self, name)
 		self._newLine = newline
 
 	def _get_newLine(self):
-		return self.newLine
+		return self._newLine
 
 	def _set_newLine(self, newline):
 		self._newLine = newline
-		
+
 	@abc.abstractmethod
 	def prepareCommand(self):
 		"""trying to make File abstract by making prepareCommand abstract"""
 
-	newLine = property(_get_newLine, _set_newLine)
-	
+	NewLine = property(_get_newLine, _set_newLine)
