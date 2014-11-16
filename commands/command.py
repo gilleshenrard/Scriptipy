e29b04f601d953e79f407c5b02a95ca2ed5fe4fd
Gilles Henrard <gilleshenrard@users.noreply.github.com>
2014-11-16 05:04:55 +0100

    Replaced whole file by something functional

--- commands/command.py
+++ commands/command.py
@@ -2,36 +2,38 @@ from scriptable import *
 
 class Command(Scriptable):
 	"""Defines specific commands (without formatting)
-	Defines the final command for the tree"""
+	Is described by :
+	Name : Command to execute
+	Comment : Eventual comment to provide
+	Final : Command to execute (same as Name, for a specific command)"""
 	
-	def __init__(self):
+	def __init__(self, name="", comment=""):
 		"""Creates an empty specific command"""
-		
-		Scriptable.__init__(self)
-		self.finalCommand = ""
-	
-	def __init__(self, name):
-		"""Creates a specific command"""
 
-		Scriptable.__init__(self, name)
+		Scriptable.__init__(self, name, comment)
 		self._finalCommand = ""
 
 	def _set_finalCommand(self, final):
-		self.finalCommand = final
+		self._finalCommand = final
+
+	def _get_finalCommand(self):
+		self.prepareCommand()
+		return self._finalCommand
 
 	def prepareCommand(self):
-		return self._finalCommand
+		"""Prepares the command to be executed"""
+		self._finalCommand=self._name
 		
 	def __str__(self):
-		return self._comment
+		"""Provides some informations on what to execute"""
+		return "will execute {}".format(self._name)
 
-	Final = property(_set_finalCommand, prepareCommand)
+	Final = property(_get_finalCommand, _set_finalCommand)
 
 #Test the constructors
 if __name__ == "__main__":
 	test = Command("Some command", "Just a command")
 	print(test.Name)
-	print(test.Final)
 	print(test.Final)
 	test.Comment="This is a comment"
 	print(test)
