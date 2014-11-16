51010d5ee5243186d6ba6d54d058ff32febc7595
Gilles Henrard <gilleshenrard@users.noreply.github.com>
2014-11-16 17:11:25 +0100

    Created osspecific.py

---
+++ commands/osspecific.py
@@ -0,0 +1,37 @@
+import abc
+from command import *
+from osspecifications import *
+
+class OSSpecific(Command, metaclass=abc.ABCMeta):
+	"""Defines the base class to handle OS specific commands (packages, repos, ...)"""
+
+	def __init__(self, name="", act="", opt=""):
+		Command.__init__(self, name)
+		self._options = opt
+		self._action = act
+
+	def _set_options(self, opt=""):
+		self._options = opt
+
+	def _get_options(self):
+		return self._options
+
+	def _set_action(self, act=""):
+		self._action = act
+
+	def _get_action(self):
+		return self._action
+
+	def _set_OS(self, osspec):
+		self._OS = osspec
+
+	def _get_OS(self):
+		return self._OS
+
+	@abc.abstractmethod
+	def __str__(self):
+		"""Just to make sure this is abstract"""
+
+	Options = property(_get_options, _set_options)
+	Action = property(_get_action, _set_action)
+	OS = property(_get_OS, _set_OS)
