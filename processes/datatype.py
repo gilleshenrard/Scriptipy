872ca124c4f72cde25db6662988c4f9e87ae6e83
Gilles Henrard <gilleshenrard@users.noreply.github.com>
2014-11-17 16:18:55 +0100

    Created datatype.py

---
+++ processes/datatype.py
@@ -0,0 +1,11 @@
+import abc
+
+class Datatype(metaclass=abc.ABCMeta):
+	"""Defines the interface for data types (XML, plain text files, databases)"""
+
+	def __init__(self):
+		"""Builds a data type"""
+
+	@abstractmethod
+	def extractFromFile(self, filename=""):
+		"""Defines the method to implement and by which we will recover all the data from the file"""
