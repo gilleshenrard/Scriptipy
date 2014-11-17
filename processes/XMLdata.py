b010277e976645e69eadf18c0389f163a6ab5f20
Gilles Henrard <gilleshenrard@users.noreply.github.com>
2014-11-17 18:36:41 +0100

    Added empty extractFromFile() method

--- processes/XMLdata.py
+++ processes/XMLdata.py
@@ -2,3 +2,11 @@ from filetype import *
 
 class XMLdata(Filetype):
 	"""Defines the XML file handling"""
+
+	def __init__(self):
+		"""Builds a new XML handling module"""
+		
+		Filetype.__init__(self)
+
+	def extractFromFile(self):
+		"""Extracting procedure"""
