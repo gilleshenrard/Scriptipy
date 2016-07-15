489419e4945e3616436350545ea96ee3bf8cb3ff
Gilles Henrard <gilleshenrard@users.noreply.github.com>
2016-07-15 13:24:24 +0200

    Update Command.py
    - Removed links to deprecated Scriptable.py
    - Removed #!
    - Added Name (either Item name for children, or command itself)
    - Added Comment
    - Removed FinalCommand (deprecated)
    - toString now returns the command (name)
    - fullfilled the test

--- libs/command.py
+++ libs/command.py
@@ -1,40 +1,42 @@
-#! /usr/bin/python
-from scriptable import *
-
-class Command(Scriptable):
+class Command():
 	"""Defines specific commands (without formatting)
 	Is described by :
-	Name : Command to execute
-	Comment : Eventual comment to provide
-	Final : Command to execute (same as Name, for a specific command)"""
+	Name : Item name or command to execute
+	Comment : Eventual comment to provide"""
 	
-	def __init__(self, name="", comment=""):
-		"""Creates an empty specific command"""
+	def __init__(self, t_name="", t_comment=""):
+		"""Creates and initialises a new specific command"""
+		self._name = t_name
+		self._comment = t_comment
 
-		Scriptable.__init__(self, name, comment)
-		self._finalCommand = ""
+	def _set_name(self, t_name):
+		"""Specifies a value for the Item name or the command"""
+		self._name = t_name
 
-	def _set_finalCommand(self, final):
-		self._finalCommand = final
+	def _get_name(self):
+		"""Returns the Item name or the command"""
+		return self._name
 
-	def _get_finalCommand(self):
-		self.prepareCommand()
-		return self._finalCommand
+	def _set_comment(self, t_comment=""):
+		"""Specifies a comment for the command to execute (wipes it if left blank)"""
+		self._comment = t_comment
 
-	def prepareCommand(self):
-		"""Prepares the command to be executed"""
-		self._finalCommand=self.Name
-		
+	def _get_comment(self):
+		"""Returns the specified comment"""
+		return self._comment
+
 	def __str__(self):
-		"""Provides some informations on what to execute"""
-		return "will execute {}".format(self.Name)
+		"""Provides the command to execute"""
+		return self.Name
 
-	Final = property(_get_finalCommand, _set_finalCommand)
+	Name = property(_get_name, _set_name)
+	Comment = property(_get_comment, _set_comment)
 
 #Test the constructors
 if __name__ == "__main__":
 	test = Command("Some command", "Just a command")
 	print(test.Name)
-	print(test.Final)
+	print(test.Comment)
 	test.Comment="This is a comment"
+	print(test.Comment)
 	print(test)
