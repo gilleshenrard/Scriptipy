a892c9bffe0995ae3d8d954c73b416e04f2498c6
Gilles Henrard <gilleshenrard@users.noreply.github.com>
2016-07-29 16:02:45 +0200

    Update repository.py
    - fixed broken reference to osspecific
    - added os as a new constructor variable
    - removed PrepareCommand, as it is replaced by __str__
    - replaced reference to getOption(str) by Options[]
    - removed test part as there will be a unit test

--- libs/repository.py
+++ libs/repository.py
@@ -1,25 +1,11 @@
-from osspecific import *
+from .osspecific import *
 
 class Repository(OSSpecific):
 	"""Defines the behaviour to handle a repository"""
-
-	def __init__(self, name="", act="", opt=""):
+	def __init__(self, name="", act="", opt="", os=OSSpecifications()):
 		"""Defines an action on a repository"""
-
-		OSSpecific.__init__(self, name, act, opt)
-
-	def prepareCommand(self):
-		act=self.OS.getOption(self.Action)
-		self.Final="{} {} {}".format(act, self.Options, self.Name)
+		OSSpecific.__init__(self, name, act, opt, os)
 
 	def __str__(self):
-		act=self.OS.getOption(self.Action)
+		act=self.OS.Options[self.Action]
 		return "{} {} {}".format(act, self.Options, self.Name)
-
-if __name__ == "__main__":
-	test = Repository("some PPA", "Add", "-y")
-	os = OSSpecifications("ubuntu", "apt-get", {"Add": "add-apt-repository"})
-	test.OS = os
-	print(test.Name)
-	print(test.Action)
-	print(test)
