a804cd42dd58f45e56a7008c73d103709f851685
Gilles Henrard <gilleshenrard@users.noreply.github.com>
2016-07-29 14:28:56 +0200

    Update osspecifications.py
    - removed #!
    - changed constructor parameters name
    - used properties into constructor
    - Removed specific options methods (deprecated, better use built-in dictionnaries methods)
    - Renamed "SpecificOptions" to "Options", shorter and simpler
    - Removed the test part, as there will be a unit test

--- libs/osspecifications.py
+++ libs/osspecifications.py
@@ -1,11 +1,10 @@
-#! /usr/bin/python
 class OSSpecifications:
 	"""Defines specifications for different OSes"""
 
-	def __init__(self, t_name="", t_pacman="", t_opt={}):
-		self._name = t_name
-		self._packageManager = t_pacman
-		self._specificOptions = t_opt
+	def __init__(self, name="", pacman="", opt={}):
+		self.Name = name
+		self.PackageManager = pacman
+		self.Options = opt
 
 	def _set_name(self, t_name):
 		self._name = t_name
@@ -25,33 +24,9 @@ class OSSpecifications:
 	def _get_specificOptions(self):
 		return self._specificOptions
 
-	def editOption(self, key, value=""):
-		self._specificOptions[key]=value
-
-	def getOption(self, key):
-		return self._specificOptions[key]
-
-	def delOption(self, key):
-		removed=self._specificOptions.pop(key)
-		return removed
-
 	def __str__(self):
 		return "OS : {}, package manager : {}, predefined options : {}".format(self._name, self._packageManager, self._specificOptions)
 
 	Name = property(_get_name, _set_name)
 	PackageManager = property(_get_packageManager, _set_packageManager)
-	SpecificOptions = property(_get_specificOptions, _set_specificOptions)
-
-if __name__ == "__main__":
-	test = OSSpecifications("Ubuntu", "apt-get", {"update": "update"})
-	print(test.Name)
-	print(test.PackageManager)
-	print(test.SpecificOptions)
-	print(test.getOption("update"))
-	test.editOption("remove", "remove")
-	print(test.SpecificOptions)
-	test.delOption("update")
-	print(test.SpecificOptions)
-	print(test)
-	test2 = OSSpecifications("arch", "pacman")
-	print(test2.SpecificOptions)
+	Options = property(_get_specificOptions, _set_specificOptions)
