6c153178da2bac0fc829d3341b910b7b32b870dc
Gilles Henrard <gilleshenrard@users.noreply.github.com>
2016-07-29 13:54:46 +0200

    Create testReplace.py
    Unit Test for libs.replace

---
+++ testReplace.py
@@ -0,0 +1,29 @@
+from libs.replace import *
+import unittest
+
+class testReplace(unittest.TestCase):
+	"""Test Case to test out Replace"""
+	def setUp(self):
+		"""Initialise test variables"""
+		self.r=Replace()
+#
+#	TEST CONSTRUCTOR
+#
+	def test_CorrectValues_Constructor_ShouldNotFail(self):
+		"""Test correct values for constructor"""
+		try:
+			r2=Replace("fileName", "This is an old line", "This is a new line")
+			print(r2)
+		except:
+			self.fail("Correct Constructor values assignment failed!")
+#
+#	TEST OLDLINE PROPERTY
+#
+	def test_CorrectValues_OldLine_ShouldNotFail(self):
+		"""Test correct values for OldLine"""
+		try:
+			self.r.OldLine="This other old line"
+			print(self.r)
+			print(self.r.OldLine)
+		except:
+			self.fail("Correct OldLine values assignment failed!")
