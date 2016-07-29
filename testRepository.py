5fd96c864915f509c8a847acc3fd59a3d5b0cc28
Gilles Henrard <gilleshenrard@users.noreply.github.com>
2016-07-29 16:03:52 +0200

    Create testRepository.py
    Unit test to lib.repository

---
+++ testRepository.py
@@ -0,0 +1,19 @@
+from libs.repository import *
+import unittest
+
+class testRepository(unittest.TestCase):
+	"""Test Case to test out Repository"""
+	def setUp(self):
+		"""Initialise test variables"""
+		pass
+#
+#	TEST CONSTRUCTOR
+#
+	def test_Correct_Constructor_ShouldNotFail(self):
+		"""Test correct values for constructor"""
+		try:
+			os = OSSpecifications("ubuntu", "apt-get", {"Add": "add-apt-repository"})
+			self.r=Repository("some PPA", "Add", "-y", os)
+			print(self.r)
+		except:
+			self.fail("Test for correct values assignment into Repository failed!")
