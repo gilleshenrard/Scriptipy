abcb3e987dfcc6301583d41e5a130b7f8ea1f2a1
Gilles Henrard <gilleshenrard@users.noreply.github.com>
2016-07-29 16:14:32 +0200

    Update package.py
    - fixed broken reference to osspecific
    - added docstring to class definition
    - added os as a constructor parameter
    - removed PrepareCommand() -> deprecated
    - replaced getOptions() to Options[]
    - removed test part, as there will be a unittest

--- libs/package.py
+++ libs/package.py
@@ -1,33 +1,10 @@
-from osspecific import *
+from .osspecific import *
 
 class Package(OSSpecific):
-
-	def __init__(self, name="", action="", option=""):
-		OSSpecific.__init__(self, name, action, option)
-
-	def prepareCommand(self):
-		act=self.OS.getOption(self.Action)
-		self.Final = "{} {} {} {}".format(self.OS.PackageManager, act, self.Options, self.Name)
+	"""Creates a package"""
+	def __init__(self, name="", action="", option="", os=OSSpecifications()):
+		OSSpecific.__init__(self, name, action, option, os)
 
 	def __str__(self):
-		act=self.OS.getOption(self.Action)
+		act=self.OS.Options[self.Action]
 		return "{} {} {} {}".format(self.OS.PackageManager, act, self.Options, self.Name)
-
-if __name__ == "__main__":
-	os = OSSpecifications("ubuntu", "apt-get")
-	os.editOption("install", "install")
-	test = Package("python", "install", "-y")
-	test.OS=os
-	print(test.Name)
-	print(test.Action)
-	print(test.OS)
-	print(test)
-
-	os = OSSpecifications("archlinux", "pacman")
-	os.editOption("install", "-S")
-	test = Package("python", "install")
-	test.OS=os
-	print(test.Name)
-	print(test.Action)
-	print(test.OS)
-	print(test)
