9aff210ae6c948bf93bc47515ad145a600e66e77
Gilles Henrard <gilleshenrard@users.noreply.github.com>
2016-08-17 09:49:12 +0200

    Updated testOSSpecific procedure
    Now using a dummy implementation class for OSSpecific : OSSpecificNotAbstract

--- unit_tests/testOSSpecific.py
+++ unit_tests/testOSSpecific.py
@@ -1,4 +1,4 @@
-from commands.osspecific import *
+from unit_tests.osspecific_notabstract import *
 import unittest
 
 class testOSSpecific(unittest.TestCase):
@@ -6,7 +6,42 @@ class testOSSpecific(unittest.TestCase):
 #
 #	TEST CONSTRUCTOR
 #
-	def test_Instanciation_Constructor_RaisesTypeError(self):
-		"""Test instanciation for constructor"""
-		with self.assertRaises(TypeError):
-			os=OSSpecific()
+	def setUp(self):
+		"""Initialise test variables"""
+		self.o=OSSpecificNotAbstract(name="test", act="doTest", opt="-l", os=OSSpecifications())
+#
+#	TEST CONSTRUCTOR
+#
+	def test_CorrectValuesContructor_ShouldNotFail(self):
+		"""Test correct values for Command constructor"""
+		try:
+			o2=OSSpecificNotAbstract(name="test2", act="doTest2", opt="-la", os=OSSpecifications())
+		except:
+			self.fail("Test for correct Constructor values assignment failed!")
+#
+#	TEST OPTIONS PROPERTY
+#
+	def test_CorrectOptions_ShouldNotFail(self):
+		"""Test correct name value for Command"""
+		try:
+			self.o.Options="ls -l"
+		except:
+			self.fail("Test for correct Options assignment failed!")
+#
+#	TEST ACTION PROPERTY
+#
+	def test_CorrectAction_ShouldNotFail(self):
+		"""Test correct comment value for Command"""
+		try:
+			self.o.Action="Some Action"
+		except:
+			self.fail("Test for correct Action assignment failed!")
+#
+#	TEST OS PROPERTY
+#
+	def test_CorrectOS_ShouldNotFail(self):
+		"""Test correct comment value for Command"""
+		try:
+			self.o.OS=OSSpecifications(name="Ubuntu", pacman="apt-get", opt={"install":"install", "remove":"remove"})
+		except:
+			self.fail("Test for correct OS assignment failed!")
