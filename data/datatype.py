8be8eaab4501ed9febe923ae6986c21f42028836
Gilles Henrard <gilleshenrard@users.noreply.github.com>
2016-08-17 16:06:12 +0200

    Update datatype.py

--- data/datatype.py
+++ data/datatype.py
@@ -1,15 +1,16 @@
 import abc
 
-class Datatype(metaclass=abc.ABCMeta):
+class DataType(metaclass=abc.ABCMeta):
 	"""Defines the interface for data types (XML, plain text files, databases)"""
 
 	def __init__(self):
 		"""Builds a data type"""
+		pass
 
-	@abstractmethod
+	@abc.abstractmethod
 	def deserialize(self):
 		"""Defines the method to implement and by which we will recover all the data from the file"""
 
-	@abstractmethod
+	@abc.abstractmethod
 	def serialize(self, data):
 		pass
