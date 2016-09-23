0824a4713114c98951cd53c297c9c6fcc0230062
gilleshenrard <gilles.henrard@hotmail.com>
2016-09-24 00:28:37 +0200

    improved doc into Replace

--- commands/replace.py
+++ commands/replace.py
@@ -1,26 +1,30 @@
 from .append import *
 
 class Replace(Append):
-	"""Replaces a line by another in a file
-	Is defined by :
-	Name : File name
-	OldLine : Line to replace
-	NewLine : Line to replace by"""
+	"""Command to replace a line by another in a file
+	
+	:param name : File name
+	:param oldLine : Line to replace in the file
+	:param newline : New value of the line
+
+	Note:
+	Uses built-in 'sed' command, will work on every UNIX based distribution."""
 
 	def __init__(self, name="", oldline="", newline=""):
-		"""Creates and initialises a line replacement"""
+		"""Create and initialise a line replacement command."""
 		Append.__init__(self, name, newline)
 		self.OldLine = oldline
 
 	def _set_oldLine(self, oldline):
-		"""Sets the old line to replace with the new line"""
+		"""Set the old line to be replaced."""
 		self._oldLine = oldline
 
 	def _get_oldLine(self):
-		"""Returns the old line to replace with the new line"""
+		"""Return the old line to be replaced."""
 		return self._oldLine
 
 	def __str__(self):
+		"""Return the line replacement command as a string."""
 		return "sed -i 's/{}/{}/g' {}".format(self.OldLine, self.NewLine, self.Name)
 
 	OldLine = property(_get_oldLine, _set_oldLine)
