8804fc1a30321d2b3ce276cc2e658d49619c5fe9
gilleshenrard <gilles.henrard@hotmail.com>
2016-09-23 21:05:44 +0200

    created unit-test for DataType

--- unit_tests/testdatatype.py
+++ unit_tests/testdatatype.py
@@ -1,7 +1,35 @@
 from data.datatype import *
+from .datatype_noabstract import *
 import unittest
 
 class testDataType(unittest.TestCase):
-	def test_Instanciation_RaisesTypeError(self):
+	"""Test Case to test out DataType"""
+#
+#	INITIALIZATION
+#
+	def setUp(self):
+		"""Initialise test variables"""
+		self.d=DataTypeNotAbstract("test")
+#
+#	TEST CONSTRUCTOR
+#
+	def test_Instanciation_Abstract_RaisesTypeError(self):
+		"""Test correct values for Command constructor"""
 		with self.assertRaises(TypeError):
-			self.d=DataType()
+			d2=DataType()
+
+	def test_CorrectValuesContructor_ShouldNotFail(self):
+		"""Test correct values for Command constructor"""
+		try:
+			d2=DataTypeNotAbstract("test2")
+		except:
+			self.fail("Test for correct Constructor values assignment failed!")
+#
+#	TEST SOURCE PROPERTY
+#
+	def test_CorrectSource_ShouldNotFail(self):
+		"""Test correct name value for Source"""
+		try:
+			self.d.Source="test"
+		except:
+			self.fail("Test for correct Source assignment failed!")
