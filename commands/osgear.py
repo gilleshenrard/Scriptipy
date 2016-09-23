7abbd9a9495fd5a5e8585c99ec27e7530b2b90af
gilleshenrard <gilles.henrard@hotmail.com>
2016-09-24 00:37:17 +0200

    Improved doc into OSGear

--- commands/osgear.py
+++ commands/osgear.py
@@ -1,30 +1,42 @@
 class OSGear:
-	"""Defines specifications for different OSes"""
+	"""Defines the characteristics specific to the operating system
+
+	:param name: Name of the OS
+	:param pacman: Package manager called by the OS
+	:param opt: Dictionnary of the options used by the package manager (install, remove, list, ...)"""
 
 	def __init__(self, name="", pacman="", opt={}):
+		"""Define the OS characteristics."""
 		self.Name = name
 		self.PackageManager = pacman
 		self.Features = opt
 
 	def _set_name(self, name):
+        """Set the OS name."""
 		self._name = name
 
 	def _get_name(self):
+        """Return the OS name."""
 		return self._name
 
 	def _set_packageManager(self, pacman):
+        """Set the package manager OS call."""
 		self._packageManager = pacman
 
 	def _get_packageManager(self):
+        """Return the package manager OS call."""
 		return self._packageManager
 
 	def _set_features(self, opt):
+        """Set the specific options used by the package manager (install, remove, list, ...)."""
 		self._features = opt
 
 	def _get_features(self):
+        """Return the specific options used by the package manager."""
 		return self._features
 
 	def __str__(self):
+        """Return a short report of the OS as a string."""
 		return "OS : {}, package manager : {}, predefined options : {}".format(self._name, self._packageManager, self._features)
 
 	Name = property(_get_name, _set_name)
