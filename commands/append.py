dfbd0dcae2bf414f0f155a713e8521fb088202da
gilleshenrard <gilles.henrard@hotmail.com>
2016-09-24 00:22:13 +0200

    improved doc into Append

--- commands/append.py
+++ commands/append.py
@@ -1,26 +1,26 @@
 from .command import *
 
 class Append(Command):
-	"""Appends a line in a file
-	Is described by :
-	Name : File name
-	NewLine : Line to add at the end of the file"""
+	"""Command to append a line in a file
+	
+	:param name : File name
+	:param newline : Line to append to the file"""
 
 	def __init__(self, name="", newline=""):
-		"""Creates and initialises a new line appending"""
+		"""Create and initialise a line appending command."""
 		Command.__init__(self, name)
 		self.NewLine = newline
 
 	def _get_newLine(self):
-		"""Returns the new line to append to a file"""
+		"""Return the line to append."""
 		return self._newLine
 
 	def _set_newLine(self, newline):
-		"""Sets the new line to append to a file"""
+		"""Set the line to append."""
 		self._newLine = newline
 
 	def __str__(self):
-		"""Provides the command which appends the new line to the file"""
+		"""Return the line appending command as a string."""
 		return "echo {} >> {}".format(self.NewLine, self.Name)
 
 	NewLine = property(_get_newLine, _set_newLine)
