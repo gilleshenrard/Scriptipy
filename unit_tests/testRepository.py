9dd0529f09bb7724f1ceac2d63f57e2e8bd18e0b
gilleshenrard <gilles.henrard@hotmail.com>
2016-09-24 13:46:46 +0200

    added test for expected final command into Repository

--- unit_tests/testRepository.py
+++ unit_tests/testRepository.py
@@ -14,6 +14,18 @@ class testRepository(unittest.TestCase):
 		"""Test correct values for constructor (should not fail)"""
 		try:
 			os = OSGear("ubuntu", "apt-get", {"Add": "add-apt-repository"})
-			self.r=Repository("some PPA", "Adds a PPA", "Add", "-y", os)
+			r=Repository("some PPA", "Adds a PPA", "Add", "-y", os)
 		except:
 			self.fail("Test for correct values assignment into Repository failed!")
+
+	#
+	# TEST __STR__
+	#
+	def test_str_Final_ShouldNotFail(self):
+		"""Test if final result is the one expected"""
+		try:
+			os = OSGear("ubuntu", "apt-get", {"Add": "add-apt-repository"})
+			r=Repository("some PPA", "Adds a PPA", "Add", "-y", os)
+			self.assertEqual(str(r), "add-apt-repository -y some PPA")
+		except:
+			self.fail("Test for expected final command failed!")
