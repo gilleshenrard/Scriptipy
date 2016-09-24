aa7a3d6d552e828b171aa9df99e74419f45a97dc
gilleshenrard <gilles.henrard@hotmail.com>
2016-09-24 14:53:38 +0200

    simplified unit tests for Append

--- unit_tests/testAppend.py
+++ unit_tests/testAppend.py
@@ -3,31 +3,23 @@ import unittest
 
 class testAppend(unittest.TestCase):
 	"""Test Case to test out Append"""
-	def setUp(self):
-		"""Initialise test variables"""
-		self.a=Append("FileName", "This is a new line")
 
-    #
-    #	TEST CONSTRUCTOR
-    #
-	def test_Constructor_Correct_ShouldNotFail(self):
-		"""Test correct values for constructor (sould not fail)"""
-		try:
-			a2=Append("fileName", "This is a new line")
-			self.assertEqual(a2.NewLine, "This is a new line")
-		except:
-			self.fail("Correct Constructor values assignment failed!")
+	#
+	#	TEST INITIALISATION
+	#
+	def setUp(self):
+		"""Declare an Append test object with correct values (should not fail).
+		Test each given values."""
+		self.a=Append("FileName", "This is a new line")
+		self.assertEqual(self.a.NewLine, "This is a new line")
 
     #
     #	TEST NEWLINE PROPERTY
     #
 	def test_NewLine_Correct_ShouldNotFail(self):
 		"""Test correct values for NewLine (should not fail)"""
-		try:
-			self.a.NewLine="This other new line"
-			self.assertEqual(self.a.NewLine, "This other new line")
-		except:
-			self.fail("Correct NewLine values assignment failed!")
+		self.a.NewLine="This other new line"
+		self.assertEqual(self.a.NewLine, "This other new line")
 
 	#
 	# TEST __STR__
