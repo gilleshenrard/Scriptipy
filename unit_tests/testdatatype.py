497e5e601fd754e4112f81159e2d5d77397e7c63
gilleshenrard <gilles.henrard@hotmail.com>
2016-09-24 10:34:52 +0200

    Improved testing readability into DataType

--- unit_tests/testdatatype.py
+++ unit_tests/testdatatype.py
@@ -4,31 +4,34 @@ import unittest
 
 class testDataType(unittest.TestCase):
 	"""Test Case to test out DataType"""
-#
-#	INITIALIZATION
-#
+
+	#
+	#	INITIALIZATION
+	#
 	def setUp(self):
-		"""Initialise test variables"""
+		"""Initialise test variables."""
 		self.d=DataTypeNotAbstract("test")
-#
-#	TEST CONSTRUCTOR
-#
+
+	#
+	#	TEST CONSTRUCTOR
+	#
 	def test_Instanciation_Abstract_RaisesTypeError(self):
-		"""Test correct values for Command constructor"""
+		"""Test instanciation of abstract DataType (raises TypeError)"""
 		with self.assertRaises(TypeError):
 			d2=DataType()
 
-	def test_CorrectValuesContructor_ShouldNotFail(self):
-		"""Test correct values for Command constructor"""
+	def test_Contructor_Correct_ShouldNotFail(self):
+		"""Test correct values for DataType constructor (abstraction removed, should not fail)"""
 		try:
 			d2=DataTypeNotAbstract("test2")
 		except:
 			self.fail("Test for correct Constructor values assignment failed!")
-#
-#	TEST SOURCE PROPERTY
-#
-	def test_CorrectSource_ShouldNotFail(self):
-		"""Test correct name value for Source"""
+
+	#
+	#	TEST SOURCE PROPERTY
+	#
+	def test_Source_Correct_ShouldNotFail(self):
+		"""Test correct Source value (should not fail)"""
 		try:
 			self.d.Source="test"
 		except:
