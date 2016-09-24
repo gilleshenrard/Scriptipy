0d71601f9a1580bd03917abc848e096485b9b438
gilleshenrard <gilles.henrard@hotmail.com>
2016-09-24 09:33:41 +0200

    Added doc into Package

--- commands/package.py
+++ commands/package.py
@@ -1,9 +1,17 @@
 from .osspecific import *
 
 class Package(OSSpecific):
-	"""Creates a package"""
+	"""Package action command, based on the information provided into the OSGear object
+
+    :param name: Name of the package
+    :param action: Action to perform with the package (into OS dictionnary)
+    :param option: Additionnal options of the package command
+    :param os: OS specific characteristics (OSGear object)"""
+
 	def __init__(self, name="", action="", option="", os=OSGear()):
+        """Declare and initialise a package action command."""
 		OSSpecific.__init__(self, name, action, option, os)
 
 	def __str__(self):
+        """Return a package action command as a string."""
 		return "{} {} {} {}".format(self.OS.PackageManager, self.Action, self.Options, self.Name)
