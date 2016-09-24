7acaf809775adea910436a7027dfb18f69af4277
gilleshenrard <gilles.henrard@hotmail.com>
2016-09-24 15:06:15 +0200

    simplified unit tests for OSSpecific

--- unit_tests/testOSSpecific.py
+++ unit_tests/testOSSpecific.py
@@ -6,51 +6,39 @@ class testOSSpecific(unittest.TestCase):
 	"""Test Case to test out OSSpecific"""
 
 	#
-	#	INITIALIZATION
+	#	TEST INITIALISATION
 	#
 	def setUp(self):
-		"""Initialise test variables"""
+		"""Declare an Append test object with correct values (should not fail).
+		Test each given values."""
 		self.o=OSSpecificNotAbstract(name="test", act="", opt="-l", os=OSGear())
+		self.assertEqual(self.o.Action, "")
+		self.assertEqual(self.o.Options, "-l")
 
 	#
 	#	TEST CONSTRUCTOR
 	#
 	def test_Instanciation_Abstract_RaisesTypeError(self):
-		"""Test correct values for OSSpecific constructor (raises TypeError)"""
+		"""Test initialisation of OSSpecific abstract constructor (raises TypeError)"""
 		with self.assertRaises(TypeError):
 			o2=OSSpecific()
-
-	def test_Contructor_Correct_ShouldNotFail(self):
-		"""Test correct values for OSSpecific constructor (should not fail)"""
-		try:
-			o2=OSSpecificNotAbstract(name="test2", act="", opt="-la", os=OSGear())
-			self.assertEqual(o2.Action, "")
-			self.assertEqual(o2.Options, "-la")
-		except:
-			self.fail("Test for correct Constructor values assignment failed!")
 
 	#
 	#	TEST OPTIONS PROPERTY
 	#
 	def test_Options_Correct_ShouldNotFail(self):
 		"""Test correct Options value for OSSpecific (should not fail)"""
-		try:
-			self.o.Options="-l"
-			self.assertEqual(self.o.Options, "-l")
-		except:
-			self.fail("Test for correct Options assignment failed!")
+		self.o.Options="-l"
+		self.assertEqual(self.o.Options, "-l")
 
 	#
 	#	TEST ACTION PROPERTY
 	#
 	def test_Action_Correct_ShouldNotFail(self):
 		"""Test correct Action value for OSSpecific (should not fail)"""
-		try:
-			self.o.OS.Features["Some Action"]="test"
-			self.o.Action="Some Action"
-			self.assertEqual(self.o.Action, "test")
-		except:
-			self.fail("Test for correct Action assignment failed!")
+		self.o.OS.Features["Some Action"]="test"
+		self.o.Action="Some Action"
+		self.assertEqual(self.o.Action, "test")
 
 	def test_Action_Wrong_RaisesValueError(self):
 		"""Test wrong values for Action setter (raises ValueError)"""
@@ -62,8 +50,5 @@ class testOSSpecific(unittest.TestCase):
 	#
 	def test_OS_Correct_ShouldNotFail(self):
 		"""Test correct OS value for OSSpecific (should not fail)"""
-		try:
-			#Assignment testing. See OSGear testing for further information
-			self.o.OS=OSGear(name="Ubuntu", pacman="apt-get", opt={"install":"install", "remove":"remove"})
-		except:
-			self.fail("Test for correct OS assignment failed!")
+		#Assignment testing. See OSGear testing for further information
+		self.o.OS=OSGear(name="Ubuntu", pacman="apt-get", opt={"install":"install", "remove":"remove"})
