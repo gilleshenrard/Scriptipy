50485408cf6ecf2cd90d8c28c98a77697aa5ecda
gilleshenrard <gilles.henrard@hotmail.com>
2016-09-24 10:44:59 +0200

    added doc and improve test readability of JSONData

--- unit_tests/testJSONdata.py
+++ unit_tests/testJSONdata.py
@@ -2,23 +2,26 @@ from data.jsondata import *
 import unittest
 
 class testJSONData(unittest.TestCase):
-	"""Test Case to test out Append"""
+	"""Test Case to test out JSDONData"""
 	def setUp(self):
 		"""Initialise test variables"""
 		self.j=JSONData("unit_tests/test_files/jsontestdata.txt")
-#
-#	TEST CONSTRUCTOR
-#
-	def test_CorrectValues_Constructor_ShouldNotFail(self):
-		"""Test correct values for constructor"""
+
+	#
+	#	TEST CONSTRUCTOR
+	#
+	def test_Constructor_Correct_ShouldNotFail(self):
+		"""Test correct values for constructor in JSDONData (should not fail)."""
 		try:
 			j2=JSONData("fileName")
 		except:
 			self.fail("Correct Constructor values assignment failed!")
-#
-#	TEST DESERIALIZATION
-#
+
+	#
+	#	TEST DESERIALIZATION
+	#
 	def test_Deserialization_Correct_ShouldNotFail(self):
+		"""Test correct deserialisation process (should not fail)."""
 		try:
 			data=self.j.deserialize()
 			self.assertEqual(data, [{"eyes": "blue","name": "John","surname": "Doe"},34,"Test"])
@@ -26,13 +29,16 @@ class testJSONData(unittest.TestCase):
 			self.fail("Correct deserialization values failed!")
 
 	def test_Deserialization_FileDoesNotExist_RaisesValueError(self):
+		"""Test deserialisation from a file which doesn*t exist (raises ValueError)."""
 		with self.assertRaises(ValueError):
 			self.j.Source="filedoesnotexist.txt"
 			self.j.deserialize()
-#
-#	TEST SERIALIZATION
-#
+
+	#
+	#	TEST SERIALIZATION
+	#
 	def test_Serialization_Correct_ShouldNotFail(self):
+		"""Test correct serialisation process (should not fail)."""
 		try:
 			data=[{"eyes": "green","name": "John","surname": "Smith"},35,"Test2"]
 			self.j.Source="unit_tests/test_files/jsontestdata2.txt"
