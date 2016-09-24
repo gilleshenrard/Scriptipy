3964bbb3e310d9951480d668ba0a4f5259980ca0
gilleshenrard <gilles.henrard@hotmail.com>
2016-09-25 01:19:50 +0200

    wrote first test implementation for XML deserialization (to complete)

--- unit_tests/testXMLdata.py
+++ unit_tests/testXMLdata.py
@@ -10,5 +10,12 @@ class testXMLData(unittest.TestCase):
 	def setUp(self):
 		"""Declare a XMLData test object with correct values (should not fail).
 		Test each given values."""
-		self.x=XMLdata("filename")
-		self.assertEqual(self.x.Source, "filename")
+		self.x=XMLdata("unit_tests/test_files/xmltestdata.xml")
+		self.assertEqual(self.x.Source, "unit_tests/test_files/xmltestdata.xml")
+
+	#
+	# 	TEST DESERIALIZATION
+	#
+	def test_Deserialization_Correct_ShouldNotFail(self):
+		"""Test correct deserialisation process (should not fail)."""
+		self.x.deserialize()
