ddc9aedd06dcf93a9bf9bcc6cc75e23178f469a2
gilleshenrard <gilles.henrard@hotmail.com>
2016-09-24 10:49:42 +0200

    improved test readability of XMLData

--- unit_tests/testXMLdata.py
+++ unit_tests/testXMLdata.py
@@ -6,11 +6,12 @@ class testXMLData(unittest.TestCase):
 	def setUp(self):
 		"""Initialise test variables"""
 		self.x=XMLdata("test")
-#
-#	TEST CONSTRUCTOR
-#
-	def test_CorrectValues_Constructor_ShouldNotFail(self):
-		"""Test correct values for constructor"""
+
+	#
+	#	TEST CONSTRUCTOR
+	#
+	def test_Constructor_Correct_ShouldNotFail(self):
+		"""Test correct values for XMLData constructor (should not fail)."""
 		try:
 			x2=XMLdata("fileName")
 		except:
