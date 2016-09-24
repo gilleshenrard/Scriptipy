727fed53c77479aed0d921537663ba812259cc56
gilleshenrard <gilles.henrard@hotmail.com>
2016-09-24 14:56:47 +0200

    simplified unit tests for Replace

--- unit_tests/testReplace.py
+++ unit_tests/testReplace.py
@@ -3,31 +3,23 @@ import unittest
 
 class testReplace(unittest.TestCase):
 	"""Test Case to test out Replace"""
-	def setUp(self):
-		"""Initialise test variables"""
-		self.r=Replace("fileName", "This is an old line", "This is a new line")
 
-    #
-    #	TEST CONSTRUCTOR
-    #
-	def test_Constructor_Correct_ShouldNotFail(self):
-		"""Test correct values for constructor (should not fail)"""
-		try:
-			r2=Replace("fileName", "This is an old line", "This is a new line")
-			self.assertEqual(r2.OldLine, "This is an old line")
-		except:
-			self.fail("Correct Constructor values assignment failed!")
+	#
+	#	TEST INITIALISATION
+	#
+	def setUp(self):
+		"""Declare a Replace test object with correct values (should not fail).
+		Test each given values."""
+		self.r=Replace("fileName", "This is an old line", "This is a new line")
+		self.assertEqual(self.r.OldLine, "This is an old line")
 
     #
     #	TEST OLDLINE PROPERTY
     #
 	def test_OldLine_Correct_ShouldNotFail(self):
 		"""Test correct values for OldLine (should not fail)"""
-		try:
-			self.r.OldLine="This other old line"
-			self.assertEqual(self.r.OldLine, "This other old line")
-		except:
-			self.fail("Correct OldLine values assignment failed!")
+		self.r.OldLine="This other old line"
+		self.assertEqual(self.r.OldLine, "This other old line")
 
 	#
 	# TEST __STR__
