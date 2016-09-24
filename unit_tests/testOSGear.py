688402183d957b58029e1f3398ab6a77f43089d3
gilleshenrard <gilles.henrard@hotmail.com>
2016-09-24 13:10:51 +0200

    improved test readability for OSGear

--- unit_tests/testOSGear.py
+++ unit_tests/testOSGear.py
@@ -6,44 +6,48 @@ class testOSGear(unittest.TestCase):
 	def setUp(self):
 		"""Initialise test variables"""
 		self.os=OSGear()
-#
-#	TEST CONSTRUCTOR
-#
-	def test_CorrectValues_Contructor_ShouldNotFail(self):
-		"""Test correct values for OSGear constructor"""
+
+	#
+	#	TEST CONSTRUCTOR
+	#
+	def test_Contructor_Correct_ShouldNotFail(self):
+		"""Test correct values for OSGear constructor (should not fail)."""
 		try:
 			os2=OSGear("Ubuntu", "apt-get", {"update": "update"})
 		except:
 			self.fail("Test for correct Constructor values assignment failed!")
-#
-#	TEST NAME PROPERTY
-#
-	def test_Correct_Name_ShouldNotFail(self):
-		"""Test correct name value for Command"""
+
+	#
+	#	TEST NAME PROPERTY
+	#
+	def test_Name_Correct_ShouldNotFail(self):
+		"""Test correct value for name into OSGear (should not fail)."""
 		try:
 			self.os.Name="Fedora Gnome"
 		except:
 			self.fail("Test for correct Name assignment failed!")
-#
-#	TEST PACKAGEMANAGER PROPERTY
-#
-	def test_Correct_PackageManager_ShouldNotFail(self):
-		"""Test correct PackageManager value for Command"""
+
+	#
+	#	TEST PACKAGEMANAGER PROPERTY
+	#
+	def test_PackageManager_Correct_ShouldNotFail(self):
+		"""Test correct PackageManager value for OSGear (should not fail)."""
 		try:
 			self.os.PackageManager="dnf"
 		except:
 			self.fail("Test for correct PackageManager assignment failed!")
-#
-#	TEST OPTIONS PROPERTY
-#
-	def test_Correct_Options_ShouldNotFail(self):
-		"""Test correct Options value for Command"""
+
+	#
+	#	TEST OPTIONS PROPERTY
+	#
+	def test_Options_Correct_ShouldNotFail(self):
+		"""Test correct Options value for OSGear (should not fail)."""
 		try:
 			self.os.Features["update"]="update"
 		except:
 			self.fail("Test for correct Options assignment failed!")
 
-	def test_RemoveWrongKey_Options_RaisesKeyError(self):
-		"""Test wrong key removal into Options"""
+	def test_Options_RemoveWrongKey_RaisesKeyError(self):
+		"""Test wrong key removal into Options (raises KeyError)."""
 		with self.assertRaises(KeyError):
 			self.os.Features.pop("WrongOption")
