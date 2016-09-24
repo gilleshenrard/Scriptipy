43d758669b6720b114a69ad0ee8d667a4fcc3d9d
gilleshenrard <gilles.henrard@hotmail.com>
2016-09-24 15:08:42 +0200

    simplified unit tests for Package

--- unit_tests/testPackage.py
+++ unit_tests/testPackage.py
@@ -3,21 +3,15 @@ import unittest
 
 class testPackage(unittest.TestCase):
 	"""Test Case to test out Package"""
-	def setUp(self):
-		"""Initialise test variables"""
-		os = OSGear("ubuntu", "apt-get", {"install" : "install"})
-		self.p = Package("python", "Installs python", "install", "-y", os)
 
 	#
-	#	TEST CONSTRUCTOR
+	#	TEST INITIALISATION
 	#
-	def test_Constructor_Correct_ShouldNotFail(self):
-		"""Test correct values for Package constructor (should not fail)"""
-		try:
-			os = OSGear("ubuntu", "apt-get", {"install" : "install"})
-			test = Package("python", "Installs python", "install", "-y", os)
-		except:
-			self.fail("Test for correct values assignment into Package failed!")
+	def setUp(self):
+		"""Declare an Append test object with correct values (should not fail).
+		Test each given values."""
+		os = OSGear("ubuntu", "apt-get", {"Installation" : "install"})
+		self.p = Package("python", "Installs python", "Installation", "-y", os)
 
 	#
 	# TEST __STR__
