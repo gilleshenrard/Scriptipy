af3af1430160ca81ca1fc56fca6aaa02aa4e3152
gilleshenrard <gilles.henrard@hotmail.com>
2016-09-24 15:25:24 +0200

    simplified unit tests for JSONData

--- unit_tests/testJSONdata.py
+++ unit_tests/testJSONdata.py
@@ -3,30 +3,23 @@ import unittest
 
 class testJSONData(unittest.TestCase):
 	"""Test Case to test out JSDONData"""
-	def setUp(self):
-		"""Initialise test variables"""
-		self.j=JSONData("unit_tests/test_files/jsontestdata.txt")
 
 	#
-	#	TEST CONSTRUCTOR
+	#	TEST INITIALISATION
 	#
-	def test_Constructor_Correct_ShouldNotFail(self):
-		"""Test correct values for constructor in JSDONData (should not fail)."""
-		try:
-			j2=JSONData("fileName")
-		except:
-			self.fail("Correct Constructor values assignment failed!")
+	def setUp(self):
+		"""Declare a JSONData test object with correct values (should not fail).
+		Test each given values."""
+		self.j=JSONData("unit_tests/test_files/jsontestdata.txt")
+		self.assertEqual(self.j.Source, "unit_tests/test_files/jsontestdata.txt")
 
 	#
 	#	TEST DESERIALIZATION
 	#
 	def test_Deserialization_Correct_ShouldNotFail(self):
 		"""Test correct deserialisation process (should not fail)."""
-		try:
-			data=self.j.deserialize()
-			self.assertEqual(data, [{"eyes": "blue","name": "John","surname": "Doe"},34,"Test"])
-		except:
-			self.fail("Correct deserialization values failed!")
+		data=self.j.deserialize()
+		self.assertEqual(data, [{"eyes": "blue","name": "John","surname": "Doe"},34,"Test"])
 
 	def test_Deserialization_FileDoesNotExist_RaisesValueError(self):
 		"""Test deserialisation from a file which doesn*t exist (raises ValueError)."""
@@ -39,9 +32,6 @@ class testJSONData(unittest.TestCase):
 	#
 	def test_Serialization_Correct_ShouldNotFail(self):
 		"""Test correct serialisation process (should not fail)."""
-		try:
-			data=[{"eyes": "green","name": "John","surname": "Smith"},35,"Test2"]
-			self.j.Source="unit_tests/test_files/jsontestdata2.txt"
-			self.j.serialize(data)
-		except:
-			self.fail("Correct serialization values failed!")
+		data=[{"eyes": "green","name": "John","surname": "Smith"},35,"Test2"]
+		self.j.Source="unit_tests/test_files/jsontestdata2.txt"
+		self.j.serialize(data)
