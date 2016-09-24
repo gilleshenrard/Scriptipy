d596c9c55f8ec2c73cd35629c2fb5d8b2709ad31
gilleshenrard <gilles.henrard@hotmail.com>
2016-09-24 10:40:04 +0200

    added doc to JSONData

--- data/jsondata.py
+++ data/jsondata.py
@@ -3,10 +3,20 @@ from json import loads, dumps
 from os import path
 
 class JSONData(DataType):
+	"""Command JSON data (de)serialisation
+	:param source: JSON source file name"""
+
 	def __init__(self, source=""):
+		"""Create and initialise a JSON (de)serialisation process."""
 		DataType.__init__(self, source)
 
 	def deserialize(self):
+		"""Deserialise data from a JSON data file.
+		Test if file exists at the source name.
+
+		Exceptions:
+		Raises ValueError if the file is not found"""
+
 		if path.exists(self.Source):
 			with open(self.Source, "r") as file:
 				return loads(file.read())
@@ -14,5 +24,8 @@ class JSONData(DataType):
 			raise ValueError("File {} not found".format(self.Source))
 
 	def serialize(self, data):
+		"""Serialise data to a file with JSON formatting.
+		If the file exists, erase all the content first."""
+
 		with open(self.Source, "w") as file:
 			file.write(dumps(data, sort_keys=True, indent=4))
