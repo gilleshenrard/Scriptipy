0bbcb5185739f0cf6e1b59782ab8dc5de63efa7d
gilleshenrard <gilles.henrard@hotmail.com>
2016-09-24 10:10:52 +0200

    improved readability for unit test of OSSpecific

--- unit_tests/testOSSpecific.py
+++ unit_tests/testOSSpecific.py
@@ -1,58 +1,63 @@
-from unit_tests.not_abstract.osspecific_notabstract import *
+from .not_abstract.osspecific_notabstract import *
 from commands.osspecific import *
 import unittest
 
 class testOSSpecific(unittest.TestCase):
 	"""Test Case to test out OSSpecific"""
-#
-#	INITIALIZATION
-#
+
+	#
+	#	INITIALIZATION
+	#
 	def setUp(self):
 		"""Initialise test variables"""
 		self.o=OSSpecificNotAbstract(name="test", act="", opt="-l", os=OSGear())
-#
-#	TEST CONSTRUCTOR
-#
+
+	#
+	#	TEST CONSTRUCTOR
+	#
 	def test_Instanciation_Abstract_RaisesTypeError(self):
-		"""Test correct values for Command constructor"""
+		"""Test correct values for OSSpecific constructor (raises TypeError)"""
 		with self.assertRaises(TypeError):
 			o2=OSSpecific()
 
-	def test_CorrectValuesContructor_ShouldNotFail(self):
-		"""Test correct values for Command constructor"""
+	def test_Contructor_Correct_ShouldNotFail(self):
+		"""Test correct values for OSSpecific constructor (should not fail)"""
 		try:
 			o2=OSSpecificNotAbstract(name="test2", act="", opt="-la", os=OSGear())
 		except:
 			self.fail("Test for correct Constructor values assignment failed!")
-#
-#	TEST OPTIONS PROPERTY
-#
-	def test_CorrectOptions_ShouldNotFail(self):
-		"""Test correct name value for Command"""
+
+	#
+	#	TEST OPTIONS PROPERTY
+	#
+	def test_Options_Correct_ShouldNotFail(self):
+		"""Test correct Options value for OSSpecific (should not fail)"""
 		try:
 			self.o.Options="ls -l"
 		except:
 			self.fail("Test for correct Options assignment failed!")
-#
-#	TEST ACTION PROPERTY
-#
-	def test_CorrectAction_ShouldNotFail(self):
-		"""Test correct comment value for Command"""
+
+	#
+	#	TEST ACTION PROPERTY
+	#
+	def test_Action_Correct_ShouldNotFail(self):
+		"""Test correct Action value for OSSpecific (should not fail)"""
 		try:
 			self.o.OS.Features["Some Action"]="test"
 			self.o.Action="Some Action"
 		except:
 			self.fail("Test for correct Action assignment failed!")
 
-	def test_Action_RaisesValueError(self):
-		"""Test wrong values for Action setter"""
+	def test_Action_Wrong_RaisesValueError(self):
+		"""Test wrong values for Action setter (raises ValueError)"""
 		with self.assertRaises(ValueError):
 			self.o.Action="Wrong Action"
-#
-#	TEST OS PROPERTY
-#
-	def test_CorrectOS_ShouldNotFail(self):
-		"""Test correct comment value for Command"""
+
+	#
+	#	TEST OS PROPERTY
+	#
+	def test_OS_Correct_ShouldNotFail(self):
+		"""Test correct OS value for OSSpecific (should not fail)"""
 		try:
 			self.o.OS=OSGear(name="Ubuntu", pacman="apt-get", opt={"install":"install", "remove":"remove"})
 		except:
