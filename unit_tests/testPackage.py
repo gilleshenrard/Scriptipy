bfcc201fa5e3c87557f2bc9470b777d725c5d7e9
gilleshenrard <gilles.henrard@hotmail.com>
2016-09-24 14:03:27 +0200

    simplified test for expected final command into Package

--- unit_tests/testPackage.py
+++ unit_tests/testPackage.py
@@ -5,6 +5,8 @@ class testPackage(unittest.TestCase):
 	"""Test Case to test out Package"""
 	def setUp(self):
 		"""Initialise test variables"""
+		os = OSGear("ubuntu", "apt-get", {"install" : "install"})
+		self.p = Package("python", "Installs python", "install", "-y", os)
 		pass
 
 	#
@@ -23,9 +25,4 @@ class testPackage(unittest.TestCase):
 	#
 	def test_str_Final_ShouldNotFail(self):
 		"""Test if final result is the one expected"""
-		try:
-			os = OSGear("ubuntu", "apt-get", {"install" : "install"})
-			test = Package("python", "Installs python", "install", "-y", os)
-			self.assertEqual(str(test), "apt-get install -y python")
-		except:
-			self.fail("Test for expected final command failed!")
+		self.assertEqual(str(self.p), "apt-get install -y python")
