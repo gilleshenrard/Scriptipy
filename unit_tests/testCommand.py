562b6922cde89ffa0339a79c7bd39d8cb640b346
gilleshenrard <gilles.henrard@hotmail.com>
2016-09-24 09:39:45 +0200

    improved readability of unit test for Command

--- unit_tests/testCommand.py
+++ unit_tests/testCommand.py
@@ -6,29 +6,32 @@ class testCommand(unittest.TestCase):
 	def setUp(self):
 		"""Initialise test variables"""
 		self.c=Command()
-#
-#	TEST CONSTRUCTOR
-#
-	def test_CorrectValuesContructor_ShouldNotFail(self):
-		"""Test correct values for Command constructor"""
+
+    #
+    #	TEST CONSTRUCTOR
+    #
+	def test_Contructor_Correct_ShouldNotFail(self):
+		"""Test correct values for Command constructor (should not fail)"""
 		try:
 			c2=Command("top", "Prints a real time list of processes")
 		except:
 			self.fail("Test for correct Constructor values assignment failed!")
-#
-#	TEST NAME PROPERTY
-#
-	def test_CorrectName_ShouldNotFail(self):
-		"""Test correct name value for Command"""
+
+    #
+    #	TEST NAME PROPERTY
+    #
+	def test_Name_Correct_ShouldNotFail(self):
+		"""Test correct name value for Command (should not fail)"""
 		try:
 			self.c.Name="ls -l"
 		except:
 			self.fail("Test for correct Name assignment failed!")
-#
-#	TEST COMMENT PROPERTY
-#
-	def test_CorrectComment_ShouldNotFail(self):
-		"""Test correct comment value for Command"""
+
+    #
+    #	TEST COMMENT PROPERTY
+    #
+	def test_Comment_Correct_ShouldNotFail(self):
+		"""Test correct comment value for Command (should not fail)"""
 		try:
 			self.c.Comment="Some comment"
 		except:
