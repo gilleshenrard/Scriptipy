e82ac98e77fcc04d72d16ba87dc503d4f8fb7a46
gilleshenrard <gilles.henrard@hotmail.com>
2016-09-24 09:33:59 +0200

    Added doc into Repository

--- commands/repository.py
+++ commands/repository.py
@@ -1,11 +1,18 @@
 from .osspecific import *
 
 class Repository(OSSpecific):
-	"""Defines the behaviour to handle a repository"""
+	"""Repository action command, based on the information provided into the OSGear object
+
+    :param name: Name of the repository
+    :param action: Action to perform with the repository (into OS dictionnary)
+    :param option: Additionnal options of the repository command
+    :param os: OS specific characteristics (OSGear object)"""
+
 	def __init__(self, name="", act="", opt="", os=OSGear()):
-		"""Defines an action on a repository"""
+        """Declare and initialise a repository action command."""
 		OSSpecific.__init__(self, name, act, opt, os)
 
 	def __str__(self):
+        """Return a repository action command as a string."""
 		act=self.OS.Options[self.Action]
 		return "{} {} {}".format(act, self.Options, self.Name)
