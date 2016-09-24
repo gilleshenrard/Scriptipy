9657388d0c415eb7ca483e3c93d3e8381080e13e
gilleshenrard <gilles.henrard@hotmail.com>
2016-09-24 10:03:54 +0200

    fixed parameter issues into OSSpecificNotAbstract

--- unit_tests/not_abstract/osspecific_notabstract.py
+++ unit_tests/not_abstract/osspecific_notabstract.py
@@ -1,8 +1,8 @@
 from commands.osspecific import *
 
 class OSSpecificNotAbstract(OSSpecific):
-	def __init__(self, name="", act="", opt="", os=OSGear()):
-		OSSpecific.__init__(self, name, act, opt, os)
+	def __init__(self, name="", comment="", act="", opt="", os=OSGear()):
+		OSSpecific.__init__(self, name, comment, act, opt, os)
 
 	def __str__(self):
 		pass
